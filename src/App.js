47ce8c40685ecc37a47fa569863fb9b1dad9f6a2
Lefti90 <55137561+Lefti90@users.noreply.github.com>
2022-06-17 00:35:31 +0300

    1d tapahtumankasittely revisited

--- src/App.js
+++ src/App.js
@@ -1,50 +1,17 @@
 import { useState } from 'react'
 
-const History = (props) => {
-  if (props.allClicks.length === 0) {
-    return (
-      <div>
-        the app is used by pressing the buttons
-      </div>
-    )
-  }
-  return (
-    <div>
-      button press history: {props.allClicks.join(' ')}
-    </div>
-  )
-}
+const App = (props) => {
+  const [value, setValue] = useState(10)
 
-const Button = ({ handleClick, text }) => (
-  <button onClick={handleClick}>
-    {text}
-  </button>
-)
-
-const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
-
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    setLeft(left + 1)
-  }
-
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    setRight(right + 1)
+  const handleClick = () => {
+    console.log('clicked the button')
+    setValue(0)
   }
 
   return (
     <div>
-      <div>
-        {left}
-        <Button handleClick={handleLeftClick} text='left' />
-        <Button handleClick={handleRightClick} text='right' />
-        {right}
-        <History allClicks={allClicks} />
-      </div>
+      {value}
+      <button onClick={handleClick}>button</button>
     </div>
   )
 }
