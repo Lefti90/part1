54844df65f1ac2f88b6b0cc89871b5c027f8858a
Lefti90 <55137561+Lefti90@users.noreply.github.com>
2022-06-16 23:18:37 +0300

    1c start - ei toimi

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
-const Hello = (props) => {
-  return (
-    <div>
-      <p>Hello {props.name}, you are {props.age} years old</p>
-    </div>
-  )
-}
-
-const Footer = () => {
-  return (
-    <div>
-      greeting app created by 
-      <a href="https://github.com/mluukkai">mluukkai</a>
-    </div>
-  )
-}
-
 const App = () => {
-  const nimi = 'Pekka'
-  const ika = 10
+  props = {
+    name: 'Maya',
+    age: 36,
+  }
   return (
     <div>
       <h1>Greetings</h1>
-      <Hello name ="Maya" age={26+10}/>
-      <Hello name ={nimi} age={ika}/>
-      <Footer />
+      <Hello />
+    </div>
+  )
+}
+
+const Hello = ({ name, age }) => {
+
+  const bornYear = () => new Date().getFullYear() - age
+  
+  
+  return (
+    <div>
+      <p>Hello {name}, you are {age} years old</p>
+      <p>So you were probably born in {bornYear()}</p>
     </div>
   )
 }
